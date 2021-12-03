df66ef4fd4df3543683ac3a2f89b1eeb71676655
Maarten-Jan <maarten-jan@orangebeard.io>
2021-12-03 10:56:31 +0100

    [team-soju#3871] Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+<h1 align="center">
+  <a href="https://github.com/orangebeard-io/cucumber-java-listener">
+    <img src="https://raw.githubusercontent.com/orangebeard-io/cucumber-java-listener/master/.github/logo.png" alt="Orangebeard.io Cucumber Java Listener" height="200">
+  </a>
+  <br>Orangebeard.io Cucumber Java Listener<br>
+</h1>
+
+<h4 align="center">Orangebeard listener for the <a href="https://cucumber.io/" target="_blank" rel="noopener">Cucumber</a> Java Behavior-Driven-Development framework.</h4>
+
+<p align="center">
+  <a href="https://repo.maven.apache.org/maven2/io/orangebeard/cucumber-java-listener/">
+    <img src="https://img.shields.io/maven-central/v/io.orangebeard/cucumber-java-listener?style=flat-square"
+      alt="MVN Version" />
+  </a>
+  <a href="https://github.com/orangebeard-io/cucumber-java-listener/actions">
+    <img src="https://img.shields.io/github/workflow/status/orangebeard-io/cucumber-java-listener/release?style=flat-square"
+      alt="Build Status" />
+  </a>
+  <a href="https://github.com/orangebeard-io/cucumber-java-listener/blob/master/LICENSE.txt">
+    <img src="https://img.shields.io/github/license/orangebeard-io/cucumber-java-listener?style=flat-square"
+      alt="License" />
+  </a>
+</p>
+
+<div align="center">
+  <h4>
+    <a href="https://orangebeard.io">Orangebeard</a> |
+    <a href="#installation">Installation</a> |
+    <a href="#configuration">Configuration</a>
+  </h4>
+</div>
+
+## Installation
+
+### Install the mvn package
+
+Add the dependency to your pom:
+```xml
+<dependency>
+    <groupId>io.orangebeard</groupId>
+    <artifactId>cucumber-java-listener</artifactId>
+    <version>version</version>
+    <scope>test</scope>
+</dependency>
+```
+
+## Configuration
+Make sure your Cucumber setup makes use of <a href="https://cucumber.io/docs/cucumber/api/#junit">JUnit</a> to execute scenarios. 
+
+Add the following plugin to the list of your plugins (comma separated) to your `junit-platform.properties` in the test resources folder:
+
+```properties
+cucumber.plugin=io.orangebeard.listener.OrangebeardCucumberListener
+```
+
+Create a new file named `orangebeard.properties` in the test resources folder. Add the following properties:
+
+```properties
+orangebeard.endpoint=<ORANGEBEARD-ENDPOINT>
+orangebeard.accessToken=<XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX>
+orangebeard.project=<PROJECT_NAME>
+orangebeard.testset=<TESTSET_NAME>
+
+# optional
+orangebeard.description=<DESCRIPTION>
+orangebeard.attributes=key:value; value;
+```
+
+### Environment variables
+
+The properties above can be set as environment variables as well. Environment variables will override property values. In the environment variables, it is allowed to replace the dot by an underscore.
+for example: ```orangebeard_endpoint``` as an environment variable will work as well.
---
+++ src/main/java/io/orangebeard/listener/OrangebeardCucumberListener.java
@@ -0,0 +1,150 @@
+package io.orangebeard.listener;
+
+import io.orangebeard.client.OrangebeardClient;
+import io.orangebeard.client.OrangebeardProperties;
+import io.orangebeard.client.OrangebeardV1Client;
+import io.orangebeard.client.entity.FinishTestItem;
+import io.orangebeard.client.entity.FinishTestRun;
+import io.orangebeard.client.entity.Log;
+import io.orangebeard.client.entity.LogLevel;
+import io.orangebeard.client.entity.StartTestItem;
+import io.orangebeard.client.entity.StartTestRun;
+import io.orangebeard.client.entity.Status;
+import io.orangebeard.client.entity.TestItemType;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import io.cucumber.plugin.EventListener;
+import io.cucumber.plugin.event.DataTableArgument;
+import io.cucumber.plugin.event.EventPublisher;
+import io.cucumber.plugin.event.PickleStepTestStep;
+import io.cucumber.plugin.event.Result;
+import io.cucumber.plugin.event.TestCaseFinished;
+import io.cucumber.plugin.event.TestCaseStarted;
+import io.cucumber.plugin.event.TestRunFinished;
+import io.cucumber.plugin.event.TestRunStarted;
+import io.cucumber.plugin.event.TestStepFinished;
+import io.cucumber.plugin.event.TestStepStarted;
+
+public class OrangebeardCucumberListener implements EventListener {
+    private final OrangebeardClient orangebeardClient;
+    private final OrangebeardProperties properties;
+    private UUID testRunUUID;
+
+    private final Map<String, UUID> testItemMap = new HashMap<>();
+    private final Map<String, UUID> suiteMap = new HashMap<>();
+    private final Map<String, UUID> stepMap = new HashMap<>();
+
+    public OrangebeardCucumberListener() {
+        this.properties = new OrangebeardProperties();
+        this.orangebeardClient = new OrangebeardV1Client(
+                properties.getEndpoint(),
+                properties.getAccessToken(),
+                properties.getProjectName(),
+                properties.requiredValuesArePresent());
+    }
+
+    @Override
+    public void setEventPublisher(EventPublisher publisher) {
+        publisher.registerHandlerFor(TestRunStarted.class, this::startTestRun);
+        publisher.registerHandlerFor(TestRunFinished.class, this::finishTestRun);
+
+        publisher.registerHandlerFor(TestCaseStarted.class, this::startTestCase);
+        publisher.registerHandlerFor(TestCaseFinished.class, this::finishTestCase);
+
+        publisher.registerHandlerFor(TestStepStarted.class, this::startTestStep);
+        publisher.registerHandlerFor(TestStepFinished.class, this::finishTestStep);
+    }
+
+    private void startTestRun(TestRunStarted event) {
+        this.testRunUUID = orangebeardClient.startTestRun(new StartTestRun(properties.getTestSetName(), properties.getDescription(), properties.getAttributes()));
+    }
+
+    private void finishTestRun(TestRunFinished event) {
+        orangebeardClient.finishTestRun(testRunUUID, new FinishTestRun());
+    }
+
+    private void startTestCase(TestCaseStarted event) {
+        UUID suiteId = startSuiteIfRequired(event.getTestCase().getUri());
+        String name = getName(event.getTestCase().getKeyword(), event.getTestCase().getName());
+        UUID testItemUUID = orangebeardClient.startTestItem(suiteId, new StartTestItem(testRunUUID, name, TestItemType.TEST));
+        testItemMap.put(event.getTestCase().getName(), testItemUUID);
+    }
+
+    private void finishTestCase(TestCaseFinished event) {
+        orangebeardClient.finishTestItem(testItemMap.get(event.getTestCase().getName()), new FinishTestItem(testRunUUID, convertResult(event.getResult())));
+    }
+
+    private void startTestStep(TestStepStarted event) {
+        UUID testCaseId = testItemMap.get(event.getTestCase().getName());
+
+        if (event.getTestStep() instanceof PickleStepTestStep) {
+            PickleStepTestStep testStep = (PickleStepTestStep) event.getTestStep();
+            String stepName = getName(testStep.getStep().getKeyword(), testStep.getStep().getText());
+
+            UUID stepUUID = orangebeardClient.startTestItem(testCaseId, new StartTestItem(testRunUUID, stepName, TestItemType.STEP));
+            stepMap.put(stepName, stepUUID);
+
+            if (testStep.getStep().getArgument() instanceof DataTableArgument) {
+                DataTableArgument datatable = (DataTableArgument) testStep.getStep().getArgument();
+                orangebeardClient.log(new Log(testRunUUID, stepUUID, LogLevel.info, datatable.cells().toString()));
+            }
+        }
+    }
+
+    private void finishTestStep(TestStepFinished event) {
+        if (event.getTestStep() instanceof PickleStepTestStep) {
+            PickleStepTestStep testStep = (PickleStepTestStep) event.getTestStep();
+
+            String stepName = getName(testStep.getStep().getKeyword(), testStep.getStep().getText());
+
+            if (event.getResult().getError() != null) {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                event.getResult().getError().printStackTrace(pw);
+
+                orangebeardClient.log(new Log(testRunUUID, stepMap.get(stepName), LogLevel.error, sw.toString()));
+            }
+            orangebeardClient.finishTestItem(stepMap.get(stepName), new FinishTestItem(testRunUUID, convertResult(event.getResult())));
+        }
+    }
+
+    private UUID startSuiteIfRequired(URI uri) {
+        String[] path = uri.toString().split("/");
+        String fileName = path[path.length - 1];
+
+        UUID suiteUUID = suiteMap.get(fileName);
+
+        if (suiteUUID == null) {
+            suiteUUID = orangebeardClient.startTestItem(null, new StartTestItem(testRunUUID, fileName, TestItemType.SUITE));
+        }
+
+        suiteMap.put(fileName, suiteUUID);
+
+        return suiteUUID;
+    }
+
+    private String getName(String keyword, String name) {
+        return keyword + ": " + name;
+    }
+
+    private Status convertResult(Result result) {
+        switch (result.getStatus()) {
+            case PASSED:
+                return Status.PASSED;
+            case SKIPPED:
+            case PENDING:
+            case UNUSED:
+                return Status.SKIPPED;
+            case UNDEFINED:
+            case FAILED:
+            case AMBIGUOUS:
+                return Status.FAILED;
+        }
+        return Status.FAILED;
+    }
+}
